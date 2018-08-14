5174100324cc2ed7b59cf85e9151c034eccdb174
Bob den Otter <bob@twokings.nl>
2018-08-14 20:08:19 +0200

    Working.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,7 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use SocialNews\FrontPage\Presentation\FrontPageController;
 
 define('ROOT_DIR', dirname(__DIR__));
 
@@ -12,9 +11,47 @@ require ROOT_DIR . '/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
+$dispatcher = \FastRoute\simpleDispatcher(
+    function (\FastRoute\RouteCollector $r) {
+        $r->addRoute(
+            'GET',
+            '/',
+            'SocialNews\FrontPage\Presentation\FrontPageController#show'
+        );
+        $r->addRoute(
+            'GET',
+            '/submit',
+            'SocialNews\Submission\Presentation\SubmissionController#show'
+        );
+    }
+);
 
-$controller = new FrontPageController();
-$response = $controller->show($request);
+$routeInfo = $dispatcher->dispatch(
+    $request->getMethod(),
+    $request->getPathInfo()
+);
+
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response = new \Symfony\Component\HttpFoundation\Response(
+            'Not found',
+            404
+        );
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response = new \Symfony\Component\HttpFoundation\Response(
+            'Method not allowed',
+            405
+        );
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        [$controllerName, $method] = explode('#', $routeInfo[1]);
+        $vars = $routeInfo[2];
+        $controller = new $controllerName;
+        $response = $controller->$method($request, $vars);
+        break;
+}
+
 if (!$response instanceof Response) {
     throw new \Exception('Controller methods must return a Response object');
 }
