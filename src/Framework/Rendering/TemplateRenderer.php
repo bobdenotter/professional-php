06854a25419a27d5a62438021c31caf1efbf72e9
Bob den Otter <bob@twokings.nl>
2018-09-08 09:16:15 +0200

    reset

--- src/Framework/Rendering/TemplateRenderer.php
+++
@@ -1,8 +0,0 @@
-<?php declare (strict_types = 1);
-
-namespace SocialNews\Framework\Rendering;
-
-interface TemplateRenderer
-{
-    public function render(string $template, array $data = []) : string;
-}
