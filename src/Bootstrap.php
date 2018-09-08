6331c5d9221f5928f01f2051fef77d1c5878c88a
Bob den Otter <bob@twokings.nl>
2018-09-08 12:31:22 +0200

    Chapter 2

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 define('ROOT_DIR', dirname(__DIR__));
 require ROOT_DIR . '/vendor/autoload.php';
@@ -12,8 +13,38 @@ Debug::enable();
 
 $request = Request::createFromGlobals();
 
-$content = 'Hello ' . $request->get('name', 'visitor');
+$dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $routes = include(ROOT_DIR . '/src/Routes.php');
+    foreach ($routes as $route) {
+        $r->addRoute(...$route);
+    }
+});
 
-$response = new \Symfony\Component\HttpFoundation\Response($content);
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response = new \Symfony\Component\HttpFoundation\Response(
+            'Not found',
+            404
+        );
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response = new \Symfony\Component\HttpFoundation\Response(
+            'Method not allowed',
+            405
+        );
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        [$controllerName, $method] = explode('#', $routeInfo[1]);
+        $vars = $routeInfo[2];
+        $controller = new $controllerName;
+        $response = $controller->$method($request, $vars);
+        break;
+}
+
+if (!$response instanceof Response) {
+    throw new \Exception('Controller methods must return a Response object');
+}
+
 $response->prepare($request);
 $response->send();
