f4b78f785b42231138d48df28038b454101818fa
Bob den Otter <bob@twokings.nl>
2018-09-09 09:24:16 +0200

    Working

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -1,12 +1,14 @@
 <?php declare (strict_types = 1);
 
 use Auryn\Injector;
+use SocialNews\Framework\Dbal\ConnectionFactory;
+use SocialNews\Framework\Dbal\DatabaseUrl;
+use SocialNews\Framework\Rendering\TemplateDirectory;
 use SocialNews\Framework\Rendering\TemplateRenderer;
 use SocialNews\Framework\Rendering\TwigTemplateRendererFactory;
-use SocialNews\Framework\Rendering\TemplateDirectory;
-
 use SocialNews\FrontPage\Application\SubmissionsQuery;
 use SocialNews\FrontPage\Infrastructure\MockSubmissionsQuery;
+use Symfony\Component\VarDumper\Server\Connection;
 
 $injector = new Injector();
 
@@ -24,5 +26,16 @@ $injector->define(TemplateDirectory::class, [':rootDirectory' => ROOT_DIR]);
 $injector->alias(SubmissionsQuery::class, MockSubmissionsQuery::class);
 $injector->share(SubmissionsQuery::class);
 
+
+
+$injector->define(
+    DatabaseUrl::class,
+    [':url' => 'sqlite:///' . ROOT_DIR . '/storage/db.sqlite3']
+);
+
+$injector->delegate(Connection::class, function () use ($injector) : Connection {
+    $factory = $injector->make(ConnectionFactory::class);
+    return $factory->create();
+});
 
 return $injector;
